e5a850efba0d1f9b180e5f5e8b46660008aad8c6
mt-hoyada <zzang8713@gmail.com>
2025-05-07 09:41:16 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'dispatch-app-v1';
+const urlsToCache = [
+  '/배차앱/dispatch.html',
+  '/배차앱/manifest.json',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+}); 
